4ea273fce56e0621e468cd60fa421de49087be3a
nico <Nicols Gonzlez>
2021-02-03 19:33:58 +0100

    StringCollectiong and Tests for StringCollection

--- src/Collection/StringCollection.php
+++ src/Collection/StringCollection.php
@@ -4,30 +4,112 @@ declare(strict_types=1);
 
 namespace StraTDeS\VO\Collection;
 
+
+use ArrayAccess;
+use Countable;
+use InvalidArgumentException;
+use Iterator;
+use StraTDeS\VO\Single\String;
 use TypeError;
 
-class StringCollection
+class StringCollection implements Countable, ArrayAccess, Iterator
 {
-    /** @var string[] */
-    private array $array;
+    protected array $items;
+    protected int $position;
 
-    private function __construct(string ...$array)
+    protected function __construct()
     {
-        $this->array = $array;
+        $this->items = [];
+        $this->position = 0;
     }
 
-    /**
-     * @param array $array
-     * @return static
-     * @throws TypeError
-     */
-    public static function fromArray(array $array): self
+    public static function create(): self
     {
-        return new static(...$array);
+        return new static();
     }
 
-    public function toArray(): array
+    public function items(): array
     {
-        return $this->array;
+        return $this->items;
+    }
+
+    public function current(): string
+    {
+        return $this->items[$this->position];
+    }
+
+    public function next(): void
+    {
+        $this->position++;
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->items[$this->position]);
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet($offset): string
+    {
+        return $this->items[$offset];
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->checkValueIsValidForCollection($value);
+        $this->checkOffsetIsValidForCollection($offset);
+
+        $this->items[$offset] = $value;
+    }
+
+    public function offsetUnset($offset): void
+    {
+        unset($this->items[$offset]);
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    protected function checkValueIsValidForCollection($value): void
+    {
+        if (is_string($value) !== true) {
+            throw new TypeError('Provided value has to be string');
+        }
+    }
+
+    protected function checkOffsetIsValidForCollection($offset): void
+    {
+        if (is_int($offset) !== true) {
+            throw new TypeError('Provided offset has to be int');
+        }
+
+        if ($offset < 0) {
+            throw new InvalidArgumentException('Provided offset has to be greater than -1');
+        }
+    }
+
+    public function add($item): self
+    {
+        $this->checkValueIsValidForCollection($item);
+
+        $this->items[] = $item;
+
+        return $this;
     }
 }
--- tests/Collection/StringCollectionTest.php
+++ tests/Collection/StringCollectionTest.php
@@ -2,23 +2,105 @@
 
 namespace StraTDeS\VO\Tests\Collection;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use StraTDeS\VO\Collection\StringCollection;
 use TypeError;
 
-class StringCollectionTest extends TestCas
+class StringCollectionTest extends TestCase
 {
-    public function testItShouldThrowTypeErrorException()
+    public function testGivenValidNamesAValidNameCollectionIsReturned(): void
     {
-        static::expectException(TypeError::class);
+        // Arrange
+        $item1 = 'a';
+        $item2 = 'b';
+        $item3 = 'c';
+        $array = [$item1, $item2, $item3];
 
-        StringCollection::fromArray([1]);
+        // Act
+        $collection = StringCollection::create();
+        $collection->add($item1);
+        $collection->add($item2);
+        $collection->add($item3);
+
+        // Assert
+        $this->assertInstanceOf(StringCollection::class, $collection);
+        $this->assertEquals($item1, $collection[0]);
+        $this->assertEquals($item2, $collection[1]);
+        $this->assertEquals($item3, $collection[2]);
+        $this->assertEquals(
+            $array,
+            $collection->items()
+        );
+
+        $this->assertCount(3, $collection);
+
+        for ($i = 0; $i < 3; $i++) {
+            $this->assertEquals($array[$i], $collection[$i]);
+        }
+
+        foreach ($collection as $key => $value) {
+            $this->assertTrue(is_string($value));
+        }
     }
 
-    public function testItShouldCreateAValidVO()
+    public function testGivenAValueByOffsetReturnsAValidValue(): void
     {
-        $stringCollection = StringCollection::fromArray(['Pepe']);
+        // Arrange
+        $item1 = 'a';
 
-        static::assertEquals(['Pepe'], $stringCollection->toArray());
+        // Act
+        $collection = StringCollection::create();
+        $collection[0] = $item1;
+
+        // Assert
+        $this->assertEquals($item1, $collection[0]);
+    }
+
+    public function testGivenAnInvalidValueItThrowsAnException(): void
+    {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessage('Provided value has to be string');
+
+        $item = 1;
+
+        $collection = StringCollection::create();
+        $collection->add($item);
+    }
+
+    public function testGivenAnInvalidOffsetItThrowsAnException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Provided offset has to be greater than -1');
+
+        $item = 'a';
+
+        $collection = StringCollection::create();
+        $collection->offsetSet(-1, $item);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Provided offset has to be greater than -1');
+
+        $item = 'a';
+
+        $collection = StringCollection::create();
+        $collection->offsetSet("a", $item);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Provided offset has to be greater than -1');
+
+        $item = 'a';
+
+        $collection = StringCollection::create();
+        $collection->offsetSet(null, $item);
+
+
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessage('Provided value has to be string');
+
+        $item = null;
+
+        $collection = StringCollection::create();
+        $collection->offsetSet(0, $item);
     }
 }
